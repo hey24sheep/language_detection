4fa90a99aec0492f1ddaf3c01b0daff773ec7ded
hey24sheep <hey24sheep@gmail.com>
2022-10-22 17:30:55 +0530

    work

--- language_detector/model.py
+++ language_detector/model.py
@@ -1,10 +1,17 @@
+# We are using https://huggingface.co/papluca/xlm-roberta-base-language-detection
+# License of the model is MIT so we can use it freely
+
+import os
 import re
 
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
                           TextClassificationPipeline)
 
-from . import MODEL_PATH
+# from . import MODEL_PATH
 
+MODEL_PATH = "./language_detector/model/roberta_lang_detect"
+
+MODEL_ID = "papluca/xlm-roberta-base-language-detection"
 
 class Model:
 
@@ -34,6 +41,8 @@ class Model:
     pipeline = None
 
     def __init__(self):
+        self.download_model()
+
         if self.pipeline is None:
             print('creating pipeline')
             self.pipeline = TextClassificationPipeline(
@@ -41,6 +50,23 @@ class Model:
                     MODEL_PATH),
                 tokenizer=AutoTokenizer.from_pretrained(MODEL_PATH),
             )
+
+    def download_model(self):
+        if not os.path.isdir(MODEL_PATH):
+            print("model not found!")
+
+            print(f"downloading model {MODEL_ID}")
+
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+            model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID)
+
+            tokenizer.save_pretrained(MODEL_PATH)
+
+            model.save_pretrained(MODEL_PATH)
+
+            print(f"model saved to {MODEL_PATH}")
+
 
     def supported_languages(self):
         return self.language_lbl
